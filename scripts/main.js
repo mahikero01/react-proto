da5ca40710542422858a4658f62fc381d1175e68
mahikero <federicoparassarmiento@yahoo.com>
2016-11-24 11:36:56 -0500

    up

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,53 @@
-var HelloWorld = React.createClass({
-render: function(){
-return (
-<p>Hello World,
-<span className='label label-primary'>
-{this.props.name}
-</span>
-</p>
-);
-}
+var CommentBox = React.createClass({
+    render: function(){
+        return (
+            <div className="commentBox">
+                <h2>Comments</h2>
+                <CommentList />
+                <h2>Add a Comment</h2>
+                <CommentForm />
+            </div>
+        );
+    }
 });
+
+var CommentList = React.createClass({
+    render: function(){
+        return (
+            <div className="commentList">
+                <Comment author="Ted">First...</Comment>
+                <Comment author="Daniel">Hellow World</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function(){
+        return (
+            <div className="commentForm">
+                CommentForm
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function(){
+        return(
+            <div className="panel panel-default comment">
+                <div className="panel-heading">
+                    {this.props.author}
+                </div>
+                <div className="panel-body">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-<HelloWorld name="Daniel"/>,
-document.getElementById('main')
-)
+    <CommentBox />,
+    document.getElementById('content')
+);
