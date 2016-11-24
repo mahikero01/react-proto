48c72bb22397305b257f86ff12bcc550e3c3fbb2
mahikero <federicoparassarmiento@yahoo.com>
2016-11-24 09:23:39 -0500

    up

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var HelloWorld = React.createClass({
+render: function(){
+return (
+<p>Hello World,
+<span className='label label-primary'>
+{this.props.name}
+</span>
+</p>
+);
+}
+});
+ReactDOM.render(
+<HelloWorld name="Daniel"/>,
+document.getElementById('main')
+)
